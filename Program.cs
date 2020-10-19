6e29ae2ac8aa298ca8ccfadeea511bed75e88bf2
Joshua Below <33503583+jbelow@users.noreply.github.com>
2020-10-19 08:58:59 -0500

    added searching

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using NLog.Web;
 using System.IO;
+using System.Linq;
+
 
 namespace MediaLibrary
 {
@@ -15,7 +17,7 @@ namespace MediaLibrary
 
 
             string movieFilePath = "movies.scrubbed.csv";
-            
+
             // When trying to get the MoiveFile it comes up with "Program.cs(16,13): error CS0246: The type or namespace name 'MovieFile' could not be found (are you missing a using directive or an assembly reference?)"
             // but if I want to try and call moive it works just fine (besides for the part of how the program doesn't work because of the MoiveFile not working)
             MovieFile movieFile = new MovieFile(movieFilePath);
@@ -27,6 +29,7 @@ namespace MediaLibrary
                 // display choices to user
                 Console.WriteLine("1) Add Movie");
                 Console.WriteLine("2) Display All Movies");
+                Console.WriteLine("3) Search based on the title of a movie");
                 Console.WriteLine("Enter to quit");
                 // input selection
                 choice = Console.ReadLine();
@@ -83,7 +86,26 @@ namespace MediaLibrary
                         Console.WriteLine(m.Display());
                     }
                 }
-            } while (choice == "1" || choice == "2");
+                else if (choice == "3")
+                {
+                    string input;
+
+                    Console.WriteLine("what do you want to search for:");
+                    input = Console.ReadLine();
+
+                    var titles = movieFile.Movies.Where(m => m.title.Contains(input)).Select(m => m.title);
+                    // LINQ - Count aggregation method
+                    Console.WriteLine($"There are {titles.Count()} movies with \"{input}\" in the title:");
+                    foreach (string t in titles)
+                    {
+                        Console.WriteLine($"  {t}");
+                    }
+
+
+                }
+
+
+            } while (choice == "1" || choice == "2" || choice == "3");
 
             logger.Info("Program ended");
 
@@ -96,38 +118,38 @@ namespace MediaLibrary
 
 
 
-                // Movie movie = new Movie
-                // {
-                //     mediaId = 123,
-                //     title = "Greatest Movie Ever, The (2020)",
-                //     director = "Jeff Grissom",
-                //     // timespan (hours, minutes, seconds)
-                //     runningTime = new TimeSpan(2, 21, 23),
-                //     genres = { "Comedy", "Romance" }
-                // };
+// Movie movie = new Movie
+// {
+//     mediaId = 123,
+//     title = "Greatest Movie Ever, The (2020)",
+//     director = "Jeff Grissom",
+//     // timespan (hours, minutes, seconds)
+//     runningTime = new TimeSpan(2, 21, 23),
+//     genres = { "Comedy", "Romance" }
+// };
 
-                // Console.WriteLine(movie.Display());
+// Console.WriteLine(movie.Display());
 
-                // Album album = new Album
-                // {
-                //     mediaId = 321,
-                //     title = "Greatest Album Ever, The (2020)",
-                //     artist = "Jeff's Awesome Band",
-                //     recordLabel = "Universal Music Group",
-                //     genres = { "Rock" }
-                // };
-                // Console.WriteLine(album.Display());
+// Album album = new Album
+// {
+//     mediaId = 321,
+//     title = "Greatest Album Ever, The (2020)",
+//     artist = "Jeff's Awesome Band",
+//     recordLabel = "Universal Music Group",
+//     genres = { "Rock" }
+// };
+// Console.WriteLine(album.Display());
 
-                // Book book = new Book
-                // {
-                //     mediaId = 111,
-                //     title = "Super Cool Book",
-                //     author = "Jeff Grissom",
-                //     pageCount = 101,
-                //     publisher = "",
-                //     genres = { "Suspense", "Mystery" }
-                // };
-                // Console.WriteLine(book.Display());
+// Book book = new Book
+// {
+//     mediaId = 111,
+//     title = "Super Cool Book",
+//     author = "Jeff Grissom",
+//     pageCount = 101,
+//     publisher = "",
+//     genres = { "Suspense", "Mystery" }
+// };
+// Console.WriteLine(book.Display());
 
-                // string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
-                // logger.Info(scrubbedFile);
+// string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
+// logger.Info(scrubbedFile);
